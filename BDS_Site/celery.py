cea65059c1faeadac97b4fb3121ac6dab4f00f24
II-Luc-II <belierluc69@gmail.com>
2025-09-23 20:41:08 +0200

    debug celery 4

--- BDS_Site/celery.py
+++ BDS_Site/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 # Définir le module de paramètres Django par défaut
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BDS_Site.settings')
@@ -10,10 +11,11 @@ app = Celery('BDS_Site')
 # Charger la configuration depuis le fichier de paramètres Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Autodiscover tasks from all registered Django app configs
-app.autodiscover_tasks()
+# Autodiscover tasks dans toutes les apps déclarées dans INSTALLED_APPS
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# Configurer le broker Redis
+# Pas besoin de redéfinir broker_url ici si tu l’as déjà dans settings.py,
+# mais tu peux le garder pour être sûr :
 app.conf.update(
     broker_url='redis://127.0.0.1:6379/0',
     result_backend='redis://127.0.0.1:6379/0',
