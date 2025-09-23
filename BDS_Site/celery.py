44d6be9892a1a88f6f1749cf2888317b2b7fa894
II-Luc-II <belierluc69@gmail.com>
2025-09-23 21:37:16 +0200

    debug celery 7

--- BDS_Site/celery.py
+++ BDS_Site/celery.py
@@ -14,9 +14,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Autodiscover tasks dans toutes les apps déclarées dans INSTALLED_APPS
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# Pas besoin de redéfinir broker_url ici si tu l’as déjà dans settings.py,
-# mais tu peux le garder pour être sûr :
-app.conf.update(
-    broker_url='redis://127.0.0.1:6379/0',
-    result_backend='redis://127.0.0.1:6379/0',
-)
