a529176b2aa5c70a5973cc5f62d5f9b7ceb9d3a6
II-Luc-II <belierluc69@gmail.com>
2025-09-23 20:25:37 +0200

    debug celery

--- BDS_Site/settings.py
+++ BDS_Site/settings.py
@@ -317,7 +317,7 @@ ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 
 
 # Configuration de celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
