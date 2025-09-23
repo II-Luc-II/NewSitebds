419dfbced5f0752b10059fbfedec7fe9ba5ab44c
II-Luc-II <belierluc69@gmail.com>
2025-09-23 20:32:16 +0200

    debug celery 2

--- BDS_Site/celery.py
+++ BDS_Site/celery.py
@@ -12,10 +12,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Autodiscover tasks from all registered Django app configs
 app.autodiscover_tasks()
-app.autodiscover_tasks(['site_bds'])
 
 # Configurer le broker Redis
 app.conf.update(
-    broker_url='redis://localhost:6379/0',
-    result_backend='redis://localhost:6379/0',
+    broker_url='redis://127.0.0.1:6379/0',
+    result_backend='redis://127.0.0.1:6379/0',
 )
