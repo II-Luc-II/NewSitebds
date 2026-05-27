69f5b5ce703338c21b6077b0ee38e2f0de2f4a77
II-Luc-II <belierluc69@gmail.com>
2026-05-27 18:46:51 +0200

    modif celery task

--- site_bds/tasks.py
+++ site_bds/tasks.py
@@ -56,35 +56,20 @@ def send_mail_batch(self, subject, html_message, plain_message, recipient_batch,
 
 @shared_task(bind=True)
 def cleanup_unconfirmed_allauth_accounts(self):
-
     User = get_user_model()
 
-    expire_days = getattr(
-        settings,
-        "ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS",
-        3
-    )
-
+    expire_days = getattr(settings, "ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS", 3)
     cutoff = timezone.now() - timedelta(days=expire_days)
 
-    # IDs des users confirmés
-    verified_user_ids = EmailAddress.objects.filter(
-        verified=True
-    ).values_list("user_id", flat=True)
-
-    # Users anciens sans email vérifié
-    users_to_delete = User.objects.exclude(
-        id__in=verified_user_ids
-    ).filter(
-        date_joined__lt=cutoff
-    )
+    users_to_delete = User.objects.filter(
+        emailaddress__verified=False,
+        date_joined__lt=cutoff,
+    ).exclude(
+        emailaddress__verified=True
+    ).distinct()
 
     deleted_users_count = users_to_delete.count()
-
-    deleted_users_ids = list(
-        users_to_delete.values_list("id", flat=True)
-    )
-
+    deleted_users_ids = list(users_to_delete.values_list("id", flat=True))
     delete_result = users_to_delete.delete()
 
     return {
